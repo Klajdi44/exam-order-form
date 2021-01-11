6276cde9f03e746fba66641fe64dfd26c9874a3f
klajdi44 <klajdi1996@yahoo.gr>
2021-01-11 22:21:10 +0100

    feedback change

--- src/components/BarInfo.js
+++ src/components/BarInfo.js
@@ -1,17 +1,26 @@
-
-const BarInfo = (props) => {
-	return (
-		<div className="barInfo">
-			<div>
-				<h1>Welcome to {props.apiData[1].bar.name === 'FooBar' ? 'Foobar' : 'FooBar'}</h1>
-				<p>Here you can choose a wide variety of unique  mouthwatering beers.</p>
-			</div>
-			<div>
-				<p>We are currently</p>
-				<h2>  {new Date().getHours() > 21 ? 'Closed' : new Date().getHours() < 6 ? 'Closed' : 'Opened'}</h2>
-			</div>
-		</div>
-	);
+const BarInfo = props => {
+  return (
+    <div className='barInfo'>
+      <div>
+        <h1>
+          Welcome to{' '}
+          {props.apiData[1].bar.name === 'FooBar' ? 'Foobar' : 'FooBar'}
+        </h1>
+        <p>Here you can choose a wide variety of unique mouthwatering beers.</p>
+      </div>
+      <div>
+        <p>We are currently</p>
+        <h2>
+          {' '}
+          {new Date().getHours() > 23
+            ? 'Closed'
+            : new Date().getHours() < 6
+            ? 'Closed'
+            : 'Opened'}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default BarInfo;
